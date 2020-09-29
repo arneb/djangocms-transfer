22c9fe265a80cf1ca45d5e4500089e2d433d2cea
Arne Brodowski <mail@arnebrodowski.de>
2020-09-29 15:28:13 +0200

    make django settings import lazy

--- djangocms_transfer/__init__.py
+++ djangocms_transfer/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
-
 __version__ = '0.3.0'
 
 default_app_config = 'djangocms_transfer.apps.TranferConfig'
 
 def get_serializer_name(default='python'):
+    from django.conf import settings
     return getattr(settings, 'DJANGO_CMS_TRANSFER_SERIALIZER', default)
